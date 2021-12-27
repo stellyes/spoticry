7d08c8908f5bfbeba0222719262332e080acadcb
ryandengland <78119057+ryandengland@users.noreply.github.com>
2021-12-26 20:04:53 -0800

    Possible solution to captcha solver (Free!) added... leaving the solve_captcha() function that uses the 2captcha API

--- spoticry/verify.py
+++ spoticry/verify.py
@@ -6,20 +6,14 @@ import random
 import requests
 import spoticore
 
-
 from bs4 import BeautifulSoup
 from requests import HTTPError
 from selenium import webdriver
 from twocaptcha import TwoCaptcha
 from selenium.webdriver.common.by import By
-
-# Solve and Verify captcha using 2Captcha API
-    #
-    # Refer to https://github.com/2captcha/2captcha-python documentation
-    # for explanation of this. Frankly, idfk what this does but the 
-    # code doesn't work without it. 
-
-
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 API_KEY = 'cfdd1e0dafb83224e79a5ade1e9191a9'
@@ -31,36 +25,14 @@ REQ_URL_TR = 'https://www.spotify.com/tr/signup'
 
 WEBDRIVER = 'src/webdriver/chromedriver.exe'
 
-ZERO_BALANCE = 'ERROR_ZERO_BALANCE'
+WD_ERROR_BALANCE = 'ERROR_ZERO_BALANCE'
+WD_ERROR_FILESIZE = 'ERROR_ZERO_CAPTCHA_FILESIZE'
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 api_env = os.getenv('APIKEY_2CAPTCHA', API_KEY)
 solver = TwoCaptcha(API_KEY)
 
-
-def sign_up(user):
-    # Handles sign-up process using generated user info
-
-    # Selecting and assigning proxy
-    #proxy = user['proxy']['ip']
-
-    chrome_options = webdriver.ChromeOptions()                  # Options argument initalization
-    # chrome_options.add_argument('--proxy-server=%s' % proxy)  # Assigns proxy
-    chrome_options.add_argument('--headless')                   # Specifies GUI display, set to headless (NOGUI)
-    chrome_options.add_argument('--ignore-certificate-errors')  # Minimize console output
-    chrome_options.add_argument('--ignore-ssl-errors')          # Minimize console output
-    chrome_options.add_argument('log-level=3')                  # Minimize console output
-    
-    # Use Google Chrome webdriver to handle form filling      
-    web = webdriver.Chrome(
-        executable_path=('src/webdriver/chromedriver.exe'), 
-        options=chrome_options
-        ) 
-
-    # Open Spotify sign-up URL via webdriver
-    print(">> Loading webpage...")
-    web.get(REQ_URL_US)
-
+def solve_captcha():
     # Grab relevant recaptchaCheckboxKey from spotify signup page
     print(">> Gathering reCaptcha information...")
 
@@ -82,7 +54,7 @@ def sign_up(user):
 
     try:
         r = requests.get(request_twocaptcha)
-        time.sleep(45)
+        time.sleep(60)
 
         try:
             r.raise_for_status()
@@ -91,7 +63,7 @@ def sign_up(user):
 
         output = str(r.text)
 
-        if output in ZERO_BALANCE:
+        if output in WD_ERROR_BALANCE:
             print(">> 2Captcha Error: ERROR_ZERO_BALANCE returned\n>> Add funds to your 2Captcha account to proceed")
             sys.exit()
 
@@ -99,57 +71,78 @@ def sign_up(user):
     except Exception as e:
         print(e)
 
-           
-    sys.exit()
+
+def sign_up(user):
+    # Handles sign-up process using generated user info
+
+    # Selecting and assigning proxy
+    #proxy = user['proxy']['ip']
+
+    chrome_options = webdriver.ChromeOptions()                  # Options argument initalization
+    # chrome_options.add_argument('--proxy-server=%s' % proxy)  # Assigns proxy
+    # chrome_options.add_argument('--headless')                   # Specifies GUI display, set to headless (NOGUI)
+    chrome_options.add_argument('--ignore-certificate-errors')  # Minimize console output
+    chrome_options.add_argument('--ignore-ssl-errors')          # Minimize console output
+    chrome_options.add_argument('log-level=3')                  # Minimize console output
     
+    # Use Google Chrome webdriver to handle form filling      
+    web = webdriver.Chrome(
+        executable_path=('src/webdriver/chromedriver.exe'), 
+        options=chrome_options
+        ) 
+
+    # Open Spotify sign-up URL via webdriver
+    print(">> Loading webpage...")
+    web.get(REQ_URL_US) 
+    time.sleep(random.randint(3, 12))
 
     # Locate and fill email portion of form
-    email_input = web.find_element('//*[@id="email"]')
-    email_input.send_keys()
+    email_input = web.find_element(By.XPATH, '//*[@id="email"]')
+    email_input.send_keys(user['email'])
     time.sleep(random.randint(1, 9))
 
     # Locate and fill email confirmation portion of form
-    email_confirmation_input = web.find_element('//*[@id="confirm"]')
-    email_confirmation_input.send_keys()
+    email_confirmation_input = web.find_element(By.XPATH, '//*[@id="confirm"]')
+    email_confirmation_input.send_keys(user['email'])
     time.sleep(random.randint(1, 9))
 
     # Locate and fill password portion of form
-    password_input = web.find_element('//*[@id="password"]')
-    password_input.send_keys()
+    password_input = web.find_element(By.XPATH, '//*[@id="password"]')
+    password_input.send_keys(user['pass'])
     time.sleep(random.randint(1, 9))
 
     # Locate and fill profile name portion of form
-    username_input = web.find_element('//*[@id="displayname"]')
-    username_input.send_keys()
+    username_input = web.find_element(By.XPATH, '//*[@id="displayname"]')
+    username_input.send_keys(user['user'])
     time.sleep(random.randint(1, 9))
 
     # Locate and fill month portion of form
-    dob_month_input = web.find_element('//*[@id="month"]')
-    dob_month_input.send_keys()
+    dob_month_input = Select(web.find_element(By.XPATH, '//*[@id="month"]'))
+    dob_month_input.select_by_visible_text(user['dob']['month'])
     time.sleep(random.randint(1, 9))
 
     # Locate and fill day portion of form
-    dob_day_input = web.find_element('//*[@id="day"]')
-    dob_day_input.send_keys()
+    dob_day_input = web.find_element(By.XPATH, '//*[@id="day"]')
+    dob_day_input.send_keys(user['dob']['day'])
     time.sleep(random.randint(1, 9))
 
     # Locate and fill year portion of form
-    dob_year_input = web.find_element('//*[@id="year"]')
-    dob_year_input.send_keys()
+    dob_year_input = web.find_element(By.XPATH, '//*[@id="year"]')
+    dob_year_input.send_keys(user['dob']['year'])
     time.sleep(random.randint(1, 9))
 
     # Locate and fill gender portion of form 
     gender = user['gender']
 
     if gender == 0:
-        male = web.find_element('//*[@id="__next"]/main/div[2]/div/form/fieldset/div/div[1]/label/span[1]')
-        male.send_keys()
+        male = web.find_element(By.XPATH, '//*[@id="__next"]/main/div[2]/div/form/fieldset/div/div[1]/label/span[1]')
+        male.click()
     elif gender == 1:
-        female = web.find_element('//*[@id="__next"]/main/div[2]/div/form/fieldset/div/div[2]/label/span[1]')
-        female.send_keys()
+        female = web.find_element(By.XPATH, '//*[@id="__next"]/main/div[2]/div/form/fieldset/div/div[2]/label/span[1]')
+        female.click()
     elif gender == 2:
-        nonbinary = web.find_element('//*[@id="__next"]/main/div[2]/div/form/fieldset/div/div[3]/label/span[1]')
-        nonbinary.send_keys()
+        nonbinary = web.find_element(By.XPATH, '//*[@id="__next"]/main/div[2]/div/form/fieldset/div/div[3]/label/span[1]')
+        nonbinary.click()
     else:
         print(">> ERROR:\n>> Invalid gender input")
 
@@ -157,23 +150,15 @@ def sign_up(user):
 
     # Locate and fill marketing information portion of form
     if user['opt_in'] == 1:
-        marketing_information = web.find_element('//*[@id="__next"]/main/div[2]/div/form/div[6]/div/label/span[1]')        
-        marketing_information.send_keys()
+        marketing_information = web.find_element(By.XPATH, '//*[@id="__next"]/main/div[2]/div/form/div[6]/div/label/span[1]')        
+        marketing_information.click()
 
     time.sleep(random.randint(1, 9))   
 
-    # Solve and Verify captcha using 2Captcha API
-    #
-    # Refer to https://github.com/2captcha/2captcha-python documentation
-    # for explanation of this. Frankly, idfk what this does but the 
-    # code doesn't work without it. 
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-
-    # Grab relevant recaptchaCheckboxKey from spotify signup page
-    props = web.find_element('//*[@id="__NEXT_DATA__"]/text()')
-    props = json.loads(props)
-    print(json.dumps(props, indent=4)) 
-    sys.exit()
+    # Solve Captcha
+    WebDriverWait(web, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//*[@id=\"checkbox-container\"]/div/div/iframe")))
+    web.execute_script("arguments[0].click();", WebDriverWait(web, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']/div[@class='recaptcha-checkbox-checkmark']"))))
+    time.sleep(60)
 
 
 def main():
@@ -183,8 +168,8 @@ def main():
             "pass": 'testpass',
             "dob": {
                 "day": 1,
-                "month": 1,
-                "year": 1900
+                "month": 'January',
+                "year": 1998
             },
             "gender": 1,
             "opt_in": 1,
