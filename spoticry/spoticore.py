f22fbabc7300f1ee15bc779b66d195c5723d4926
ryandengland <78119057+ryandengland@users.noreply.github.com>
2022-02-03 19:38:44 -0800

    Added some user agent devices, picks random agent from PC list

--- spoticry/spoticore.py
+++ spoticry/spoticore.py
@@ -1,10 +1,7 @@
-from email.mime import base
 import os
 import sys
-from tempfile import TemporaryFile
 import time
 import json
-from sympy import use
 import utils
 import string
 import random
@@ -12,8 +9,6 @@ import shutil
 import pickle
 import hashlib
 import requests
-
-from fake_useragent import UserAgent
 
 from utils import bcolors
 from selenium import webdriver
@@ -27,6 +22,70 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.action_chains import ActionChains
 
+# First page has been parsed, reference this:
+# https://developers.whatismybrowser.com/useragents/explore/software_name/spotify/2
+
+USER_AGENT_PC = [
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Spotify/1.1.31.703 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Spotify/1.1.32.618 Safari/537.36",
+    "Spotify/117400631 Win32/0 (PC desktop)",
+    "Spotify/117300517 Win32/0 (PC laptop)",
+    "Spotify/117400631 Win32/0 (PC laptop)",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Spotify/1.1.33.569 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Spotify/1.1.30.658 Safari/537.36",
+
+]
+
+USER_AGENT_iOS = [
+    "Spotify/1.0",
+    "Spotify/8.6.84 iOS/15.1 (iPhone12,1)",
+    "Spotify/8.6.84 iOS/15.1.1 (iPhone13,2)",
+    "Spotify/8.6.84 iOS/15.1 (iPhone11,8)",
+    "Spotify/8.6.84 iOS/15.1 (iPhone12,8)",
+    "Spotify/8.6.84 iOS/15.1 (iPhone10,4)",
+    "Spotify/8.6.82 iOS/14.8.1 (iPhone12,1)",
+    "Spotify/8.6.94 iOS/15.1.1 (iPhone13,2)",
+    "Spotify/8.6.82 iOS/14.8.1 (iPhone13,2)",
+    "Spotify/8.6.84 iOS/15.1 (iPhone11,2)",
+    "Mozilla/5.0 (iPhone; CPU OS 12_2_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25 Spotify/856800921 (36; 2; 3)",
+    "Spotify/8.6.84 iOS/15.1.1 (iPhone13,1)",
+    "Spotify/8.6.96 iOS/15.1 (iPhone12,1)",
+    "Spotify/8.6.82 iOS/15.1 (iPhone12,1)",
+    "Spotify/8.6.84 iOS/14.8.1 (iPhone10,4)",
+    "Spotify/8.6.84 iOS/14.8.1 (iPhone13,3)",
+    "Spotify/8.6.84 iOS/14.8.1 (iPhone13,3)",
+    "Spotify/8.6.96 iOS/15.1 (iPhone12,8)",
+    "Spotify/8.6.84 iOS/14.8.1 (iPhone9,3)",
+    "Spotify/8.6.84 iOS/15.1 (iPhone9,3)",
+    "Spotify/8.6.30 iOS/14.6 (iPhone10,4)",
+    "Spotify/8.6.84 iOS/15.1 (iPhone12,5)",
+    "Spotify/8.6.96 iOS/15.1.1 (iPhone13,2)",
+    "Spotify/8.6.84 iOS/15.1.1 (iPhone13,3)",
+    "Spotify/8.6.84 iOS/15.1.1 (iPhone14,5)",
+    "Spotify/8.6.84 iOS/14.8.1 (iPhone12,1)",
+    "Spotify/8.6.82 iOS/15.1 (iPhone13,2)",
+    "Spotify/8.6.30 iOS/14.6 (iPhone12,8)",
+    "Spotify/8.6.84 iOS/14.7.1 (iPhone12,3)",
+    "Spotify/8.6.52 iOS/14.6 (iPhone12,8)",
+    "Spotify/8.6.84 iOS/15.1 (iPhone12,3)"
+]
+
+USER_AGENT_ANDROID = [
+    "Spotify/1.0",
+    "Spotify/8.6.88.1104 Android/30 (SM-A415F)",
+    "Spotify/8.6.88.1104 Android/30 (SM-G973F)",
+    "Spotify/8.6.94.306 Android/30 (M2102J20SG)",
+    "Spotify/8.6.94.306 Android/30 (SM-G975F)",
+    "Spotify/8.6.88.1104 Android/30 (SM-A525F)",
+    "Spotify/8.4.49 Android/28 (LG)",
+    "Spotify/8.6.88.1104 Android/30 (SM-A505FN)",
+    "Spotify/8.6.98.900 Android/31 (SM-G986B)",
+    "Spotify/8.4.88 Android/28 (SM-A105FN)",
+    "Spotify/8.6.88.1104 Android/30 (SM-G981B)",
+    "Spotify/8.6.74.1176 Android/30 (SM-G991B)",
+    "Spotify/8.6.84.1240 Android/30 (SM-A705FN)",
+
+]
 
 START = "start"
 RESUME = "resume"
@@ -115,15 +174,12 @@ class session():
 class userinstance():
     def __init__(self, user, site, state):
         # Options argument initalization
-        chrome_options = webdriver.ChromeOptions()     
-        ua = UserAgent()
-        userAgent = ua.random   
-        print(userAgent)        
+        chrome_options = webdriver.ChromeOptions()          
 
         # chrome_options.add_argument('--proxy-server=%s' % user['proxy']['ip'])                  # Assigns proxy
         # chrome_options.add_argument('--headless')                                             # Specifies GUI display, set to headless (NOGUI)
         chrome_options.add_argument("--mute-audio")                                             # Mute audio output
-        #chrome_options.add_argument(f'user-agent={userAgent}')
+        chrome_options.add_argument(f'user-agent={random.choice(USER_AGENT_PC)}')
         chrome_options.add_argument('--disable-blink-features=AutomationControlled')
         chrome_options.add_experimental_option('useAutomationExtension', False)
         chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -543,6 +599,7 @@ class userinstance():
 
         print(">> " + bcolors.BOLD + bcolors.OKGREEN + "Playlists imported" + bcolors.ENDC)
    
+    # Imports list of artists in storage file
     def importArtists(self):
         print(">> " + bcolors.BOLD + bcolors.OKGREEN + "Importing artists..." + bcolors.ENDC)
         imported = []
